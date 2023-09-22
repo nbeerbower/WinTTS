a42e24d9f738c76cba0af4c4574957c1369cc613
Nicholas Beerbower <nbeerbower@gmail.com>
2023-09-22 12:19:55 -0400

    Allow selecting a different audio output

--- WinTTS/Program.cs
+++ WinTTS/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Speech.Synthesis;
+using CSCore.Codecs.WAV;
+using CSCore.SoundOut;
+using CSCore.CoreAudioAPI;
 
 class Program
 {
@@ -21,7 +24,52 @@ class Program
             Console.Write("Enter text to be spoken: ");
             string text = Console.ReadLine();
 
-            synthesizer.Speak(text);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                synthesizer.SetOutputToWaveStream(memoryStream);
+                synthesizer.Speak(text);
+
+                // Reset memory stream position
+                memoryStream.Position = 0;
+
+                // List available audio devices
+                Console.WriteLine("Available Audio Devices:");
+                var deviceEnumerator = new MMDeviceEnumerator();
+                var devices = deviceEnumerator.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active);
+                int deviceIndex = 0;
+                foreach (var device in devices)
+                {
+                    Console.WriteLine($"{deviceIndex++}. {device.FriendlyName}");
+                }
+
+                // Prompt user for device selection
+                Console.Write("Select an audio device by index: ");
+                if (!int.TryParse(Console.ReadLine(), out int selectedIndex) || selectedIndex < 0 || selectedIndex >= devices.Count)
+                {
+                    Console.WriteLine("Invalid selection. Exiting...");
+                    return;
+                }
+
+                // Play the memory stream on the selected audio device using CSCore
+                using (var waveSource = new WaveFileReader(memoryStream))
+                {
+                    using (var soundOut = new WasapiOut())
+                    {
+                        soundOut.Device = devices[selectedIndex];
+                        soundOut.Initialize(waveSource);
+                        soundOut.Play();
+
+                        // Use an event to wait for the audio to finish playing
+                        bool playbackFinished = false;
+                        soundOut.Stopped += (s, e) => playbackFinished = true;
+
+                        while (!playbackFinished)
+                        {
+                            System.Threading.Thread.Sleep(100);
+                        }
+                    }
+                }
+            }
         }
     }
 }
