03f0ea4193a3df324ff6b4408b1e5e8454fda829
Nicholas Beerbower <nbeerbower@gmail.com>
2023-09-22 11:24:37 -0400

    Add project files.

---
+++ WinTTS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (SpeechSynthesizer synthesizer = new SpeechSynthesizer())
+        {
+            // Iterate available voices
+            Console.WriteLine("Available Voices:");
+            string selectedVoice = "";
+            foreach (InstalledVoice voice in synthesizer.GetInstalledVoices())
+            {
+                Console.WriteLine($"- {voice.VoiceInfo.Name}");
+                selectedVoice = voice.VoiceInfo.Name;
+            }
+            synthesizer.SelectVoice(selectedVoice);
+            Console.WriteLine("Using voice: " + selectedVoice);
+
+            // Get user input for text to be spoken
+            Console.Write("Enter text to be spoken: ");
+            string text = Console.ReadLine();
+
+            synthesizer.Speak(text);
+        }
+    }
+}
